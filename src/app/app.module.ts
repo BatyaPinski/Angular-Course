4dc5fa0dae470b5d0270e5b59312980cd1896a9c
BatyaPinski <pinskim.batya@gmail.com>
2019-04-01 00:20:33 +0300

    ssession3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponentComponent } from './book-component/book-component.component';
@@ -19,7 +19,8 @@ import { EditComponentComponent } from './edit-component/edit-component.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [BookService],
   bootstrap: [AppComponent]
